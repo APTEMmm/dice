bfa7bbd0e6f9432d53468fe21aa55856cc755ee8
APTEMmm <artemqrtz@gmail.com>
2022-06-27 21:54:49 +0300

    Added sum of dices

--- roll_the_dice.rb
+++ roll_the_dice.rb
@@ -3,14 +3,19 @@ puts 'How many dices?'
 num = gets.to_i
 
 def show_rolling_die
-  100.times do
+  10.times do
     print "#{rand(1..6)}"
     sleep 0.01
   end
   print "\n"
 end
 
+sum_of_dices = 0
+
 num.times do
   show_rolling_die
-  puts "Выпало: #{rand(1..6)}"
+  number_drawn = rand(1..6)
+  sum_of_dices += number_drawn
+  puts "Выпало: #{number_drawn}"
 end
+puts "Sum of dice: #{sum_of_dices}"
